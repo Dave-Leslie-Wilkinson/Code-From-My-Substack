3ab21b789303108da4aa176ee25914aa7519cfb9
David Wilkinson <60782691+Dave-Leslie-Wilkinson@users.noreply.github.com>
2021-10-15 12:13:41 +0200

    Update Und. & Viz. SA 2021 Elections.py

--- Und. & Viz. SA 2021 Elections.py
+++ Und. & Viz. SA 2021 Elections.py
@@ -1,12 +1,12 @@
+#Divide Paragraph Into individual Words
+print("Words:")
 def Convert(string1):
     string = " ".join(string1.split())
     li = list(string.split(" "))
     return li
 
-txt = """ What a DA government will do:
-1.1 Ensure there is clean water and a sanitary environment for
-every community.
-
+txt = """ paragraph
+example.
 """
 string_one = txt.replace("\n", " ")
 
@@ -14,8 +14,17 @@ string_one = txt.replace("\n", " ")
 list_one = Convert(string_one)
 print(list_one)
 
+
 #Count Words
+print("#################")
+print("Counted Words:")
 import collections
 
 counter = collections.Counter(list_one)
 print(counter.most_common())
+
+#Count Specific Word
+print("Specific Word:")
+word = "paragraph"
+occurrences = string_one.count(word)
+print('Number of occurrences of the word', word, occurrences)
