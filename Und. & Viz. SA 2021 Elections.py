5a525f3cfcaefe9ca66b03f5d022dcdac4e2257f
David Wilkinson <60782691+Dave-Leslie-Wilkinson@users.noreply.github.com>
2021-10-15 12:30:57 +0200

    Update Und. & Viz. SA 2021 Elections.py

--- Und. & Viz. SA 2021 Elections.py
+++ Und. & Viz. SA 2021 Elections.py
@@ -1,12 +1,13 @@
 #Divide Paragraph Into individual Words
-print("Words:")
+print("List of Words:")
 def Convert(string1):
     string = " ".join(string1.split())
     li = list(string.split(" "))
     return li
 
-txt = """ paragraph
-example.
+txt = """ What a DA government will do:
+1.1 Ensure there is clean water and a sanitary environment for
+every community.
 """
 string_one = txt.replace("\n", " ")
 
@@ -24,7 +25,11 @@ counter = collections.Counter(list_one)
 print(counter.most_common())
 
 #Count Specific Word
+print("#################")
 print("Specific Word:")
-word = "paragraph"
-occurrences = string_one.count(word)
-print('Number of occurrences of the word', word, occurrences)
+word_search = ["road", "roads", "Roads", "water", "Water", "infrastructure", "Infrastructure", "maintenance", "Maintenance", "sanitation", "Sanitation", "waste", "Waste", "energy", "Energy", "electricity", 
+"Electricity", "skills", "skill", "Skills", "Skill", "training", "Training", "education", "Education", "child", "Child", "childhood", "Childhood", "crime", "Crime", 
+"jobs", "job", "Job", "Jobs", "employment", "Employment", "corruption", "Corruption"]
+for word in word_search:
+  occurrences = string_one.count(word)
+  print(word, occurrences)
