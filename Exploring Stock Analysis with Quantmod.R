77163c2f270c02e947f658c94d545c5e569e218c
David Wilkinson <60782691+Dave-Leslie-Wilkinson@users.noreply.github.com>
2022-05-11 14:21:00 +0200

    Delete Exploring Stock Analysis with Quantmod.R

--- Exploring Stock Analysis with Quantmod.R
+++
@@ -1,9 +0,0 @@
-library(quantmod)
-getSymbols("SHOP")
-chartSeries(SHOP,
-            type="line",
-            name="Shopify Stock Price & Moving Averages",
-            subset = "2020-08-30::2021-08-30")
-addEMA(n=10, on=1, col="blue")
-addEMA(n=50, on=1, col="red")
-addEMA(n=200, on=1, col="yellow")
